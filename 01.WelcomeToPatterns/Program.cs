d8efca077500dccf2c31b68c93bc1d1166d003a0
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-12-11 19:18:53 +0300

    Преобразование class library в консоль + Program

---
+++ 01.WelcomeToPatterns/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using _01.WelcomeToPatterns.Abstract;
+
+namespace _01.WelcomeToPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallard = new MallardDuck();
+            mallard.PerformQuack();
+            mallard.PerformFly();
+
+            Console.ReadKey();
+        }
+    }
+}
