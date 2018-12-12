18696009209241b6f7bf8d410275b945e948f335
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-12-12 18:37:28 +0300

    Проверяем паттерн Наблюдатель в действии - и ничего не выводится на экран

--- 02.WeatherStation/Program.cs
+++ 02.WeatherStation/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using _02.WeatherStation.Observation;
+using _02.WeatherStation.Observation.Abstract;
+using _02.WeatherStation.Screens;
 
 namespace _02.WeatherStation
 {
@@ -6,7 +9,19 @@ namespace _02.WeatherStation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var weatherDataSubject = new WeatherDataSubject();
+
+            CurrentConditionsScreen currentScreen = new CurrentConditionsScreen(weatherDataSubject);
+            //StatisticsScreen statisticsScreen = new StatisticsScreen(weatherDataSubject);
+            //ForecastScreen forecastScreen = new ForecastScreen(weatherDataSubject);
+
+            currentScreen.Display();
+
+            weatherDataSubject.SetMeasurements(80, 65, 30.4f);
+            weatherDataSubject.SetMeasurements(82, 70, 29.2f);
+            weatherDataSubject.SetMeasurements(78, 90, 29.2f);
+
+            Console.ReadKey();
         }
     }
 }
