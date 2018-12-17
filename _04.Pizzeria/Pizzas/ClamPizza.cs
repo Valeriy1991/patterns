7e3cb4ffa354bdb889e66835d274498db339bd49
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-12-17 19:27:55 +0300

    Add new project - pattern Factory

---
+++ _04.Pizzeria/Pizzas/ClamPizza.cs
@@ -0,0 +1,6 @@
+namespace _04.Pizzeria.Pizzas
+{
+    public class ClamPizza : Pizza
+    {
+    }
+}
---
+++ _04.Pizzeria/Pizzas/Pizza.cs
@@ -0,0 +1,25 @@
+namespace _04.Pizzeria.Pizzas
+{
+    public abstract class Pizza
+    {
+        public virtual void Prepare()
+        {
+            
+        }
+
+        public virtual void Bake()
+        {
+            
+        }
+
+        public virtual void Cut()
+        {
+            
+        }
+
+        public virtual void Box()
+        {
+            
+        }
+    }
+}
