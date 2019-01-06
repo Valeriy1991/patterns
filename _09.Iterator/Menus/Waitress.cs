e75b9ccf4c17aa18d7d7f14645eb741b71191ff4
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2019-01-06 21:32:05 +0300

    Добавлена спецификация официантки

---
+++ _09.Iterator/Menus/Waitress.cs
@@ -0,0 +1,33 @@
+namespace _09.Iterator.Menus
+{
+    /// <summary>
+    /// Официантка :)
+    /// </summary>
+    public class Waitress
+    {
+        public void PrintMenu()
+        {
+            
+        }
+
+        public void PrintBreakfastMenu()
+        {
+            
+        }
+
+        public void PrintLunchMenu()
+        {
+            
+        }
+
+        public void PrintVegetarianMenu()
+        {
+            
+        }
+
+        public bool IsItemVegetarian(string name)
+        {
+            return false;
+        }
+    }
+}
