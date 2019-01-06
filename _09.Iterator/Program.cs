c203532228836f4ce5ded3a4f04ed4ae6fa385d7
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2019-01-06 21:12:40 +0300

    Добавлен новый проект для паттерна "Итератор"

---
+++ _09.Iterator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
