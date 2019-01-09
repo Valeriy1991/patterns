b7e71b29793fabcf69214b629124f0894b0ec2ce
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2019-01-09 14:40:26 +0300

    Добавил тесты, но все равно непонятно, почему возникает NRE и при выводе вегетарианского меню отображается всего 2 позиции (должно быть 13)

---
+++ _09.Composite.Tests/Iterators/CompositeIteratorTests.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+using _09.Composite.Iterators;
+using _09.Composite.Menus;
+using _09.Composite.Menus.Abstract;
+
+namespace _09.Composite.Tests.Iterators
+{
+    [ExcludeFromCodeCoverage]
+    [Trait("Category", "Unit")]
+    public class CompositeIteratorTests
+    {
+        private CompositeIterator CreateTestedComponent(IEnumerator<MenuComponent> enumerator)
+        {
+            return new CompositeIterator(enumerator);
+        }
+
+        private readonly List<MenuComponent> _menuComponents;
+        private CompositeIterator _iterator;
+
+        public CompositeIteratorTests()
+        {
+            _menuComponents = new List<MenuComponent>();
+        }
+
+        [Fact]
+        public void Current__MenuIsEmpty__ReturnNull()
+        {
+            // Arrange
+            IEnumerator<MenuComponent> enumerator = _menuComponents.GetEnumerator();
+            _iterator = CreateTestedComponent(enumerator);
+            // Act
+            var current = _iterator.Current;
+            // Assert
+            Assert.Null(current);
+        }
+        [Fact]
+        public void MoveNext__MenuIsEmpty__ReturnFalse()
+        {
+            // Arrange
+            IEnumerator<MenuComponent> enumerator = _menuComponents.GetEnumerator();
+            _iterator = CreateTestedComponent(enumerator);
+            // Act
+            var wasMoved = _iterator.MoveNext();
+            // Assert
+            Assert.False(wasMoved);
+        }
+
+        [Fact]
+        public void Current__MenuIsNotEmpty__ReturnFirstMenuItem()
+        {
+            // Arrange
+            var menuItem1 = new MenuItem("menu-item-1", "desc-1", false, 0.1m);
+            _menuComponents.Add(menuItem1);
+
+            IEnumerator<MenuComponent> enumerator = _menuComponents.GetEnumerator();
+            _iterator = CreateTestedComponent(enumerator);
+            // Act
+            var current = _iterator.Current;
+            // Assert
+            Assert.Equal(menuItem1.GetName(), current.GetName());
+        }
+        [Fact]
+        public void MoveNext__MenuIsNotEmpty__ReturnTrue()
+        {
+            // Arrange
+            var menu = new Menu("menu-1", "menu-desc-1");
+            _menuComponents.Add(menu);
+
+            IEnumerator<MenuComponent> enumerator = _menuComponents.GetEnumerator();
+            _iterator = CreateTestedComponent(enumerator);
+            // Act
+            var wasMoved = _iterator.MoveNext();
+            // Assert
+            Assert.True(wasMoved);
+        }
+
+        [Fact]
+        public void Current__MenuContains3MenuItems_MoveNext2Times__Return3rdMenuItem()
+        {
+            // Arrange
+            var menuItem1 = new MenuItem("menu-item-1", "desc-1", false, 0.1m);
+            var menuItem2 = new MenuItem("menu-item-2", "desc-2", false, 0.2m);
+            var menuItem3 = new MenuItem("menu-item-3", "desc-3", false, 0.3m);
+            _menuComponents.Add(menuItem1);
+            _menuComponents.Add(menuItem2);
+            _menuComponents.Add(menuItem3);
+
+            IEnumerator<MenuComponent> enumerator = _menuComponents.GetEnumerator();
+            _iterator = CreateTestedComponent(enumerator);
+            _iterator.MoveNext();
+            _iterator.MoveNext();
+            // Act
+            var current = _iterator.Current;
+            // Assert
+            Assert.Equal(menuItem3.GetName(), current.GetName());
+        }
+        [Fact]
+        public void MoveNext__MenuContains3MenuItems_MoveNext2Times__ReturnTrue()
+        {
+            // Arrange
+            var menuItem1 = new MenuItem("menu-item-1", "desc-1", false, 0.1m);
+            var menuItem2 = new MenuItem("menu-item-2", "desc-2", false, 0.2m);
+            var menuItem3 = new MenuItem("menu-item-3", "desc-3", false, 0.3m);
+            _menuComponents.Add(menuItem1);
+            _menuComponents.Add(menuItem2);
+            _menuComponents.Add(menuItem3);
+
+            IEnumerator<MenuComponent> enumerator = _menuComponents.GetEnumerator();
+            _iterator = CreateTestedComponent(enumerator);
+            _iterator.MoveNext();
+            _iterator.MoveNext();
+            // Act
+            var wasMoved = _iterator.MoveNext();
+            // Assert
+            Assert.True(wasMoved);
+        }
+        [Fact]
+        public void MoveNext__MenuContains3MenuItems_MoveNext3Times__ReturnFalse()
+        {
+            // Arrange
+            var menuItem1 = new MenuItem("menu-item-1", "desc-1", false, 0.1m);
+            var menuItem2 = new MenuItem("menu-item-2", "desc-2", false, 0.2m);
+            var menuItem3 = new MenuItem("menu-item-3", "desc-3", false, 0.3m);
+            _menuComponents.Add(menuItem1);
+            _menuComponents.Add(menuItem2);
+            _menuComponents.Add(menuItem3);
+
+            IEnumerator<MenuComponent> enumerator = _menuComponents.GetEnumerator();
+            _iterator = CreateTestedComponent(enumerator);
+            _iterator.MoveNext();
+            _iterator.MoveNext();
+            _iterator.MoveNext();
+            // Act
+            var wasMoved = _iterator.MoveNext();
+            // Assert
+            Assert.False(wasMoved);
+        }
+    }
+}
---
+++ _09.Composite.Tests/Menus/WaitressTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+using _09.Composite.Menus;
+using _09.Composite.Menus.Abstract;
+
+namespace _09.Composite.Tests.Menus
+{
+    [ExcludeFromCodeCoverage]
+    [Trait("Category", "Unit")]
+    public class WaitressTests
+    {
+        private Waitress CreateTestedComponent(MenuComponent menu)
+        {
+            return new Waitress(menu);
+        }
+
+        public static IEnumerable<object[]> MenuTree = new List<object[]>()
+        {
+            new object[] {CreateSimpleMenu()},
+            new object[] {CreateRealMenu()}
+        };
+
+        private static Menu CreateSimpleMenu()
+        {
+            var menuItem1 = new MenuItem("item-1", "desc-1", false, 0.1m);
+            var menuItem2 = new MenuItem("item-2", "desc-2", false, 0.2m);
+            var menuItem3 = new MenuItem("item-3", "desc-3", false, 0.3m);
+            var menuItem4 = new MenuItem("item-4", "desc-4", true, 0.4m);
+            var menuItem5 = new MenuItem("item-5", "desc-5", true, 0.5m);
+
+            var subSubMenuItem1 = new MenuItem("item-6-1-1", "desc-6-1-1", true, 0.001m);
+            var subSubMenuItem2 = new MenuItem("item-6-1-2", "desc-6-1-2", false, 0.002m);
+            var subMenu1 = new Menu("item-6-1", "desc-6-1");
+            subMenu1.Add(subSubMenuItem1);
+            subMenu1.Add(subSubMenuItem2);
+
+            var subMenuItem2 = new MenuItem("item-6-2", "desc-6-2", true, 0.02m);
+            var subMenuItem3 = new MenuItem("item-6-3", "desc-6-3", false, 0.03m);
+            var menuItem6WithSubMenu = new Menu("item-6", "desc-6");
+            menuItem6WithSubMenu.Add(subMenu1);
+            menuItem6WithSubMenu.Add(subMenuItem2);
+            menuItem6WithSubMenu.Add(subMenuItem3);
+
+            var menu = new Menu("menu", "desc");
+            menu.Add(menuItem1);
+            menu.Add(menuItem2);
+            menu.Add(menuItem3);
+            menu.Add(menuItem4);
+            menu.Add(menuItem5);
+            menu.Add(menuItem6WithSubMenu);
+
+            return menu;
+        }
+
+        private static Menu CreateRealMenu()
+        {
+            return MenuFactory.Create();
+        }
+
+        [Theory]
+        [MemberData(nameof(MenuTree))]
+        public void PrintVegetarianMenu__DoesNotThrowAnyException(Menu menu)
+        {
+            // Arrange
+            var waitress = CreateTestedComponent(menu);
+            Action act = () => waitress.PrintVegetarianMenu();
+            // Act
+            var ex = Record.Exception(act);
+            // Assert
+            Assert.Null(ex);
+        }
+    }
+}
