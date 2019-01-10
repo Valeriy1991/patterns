a12808abacfe5041e680ffc5f4efa02ef9f941c7
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2019-01-10 19:13:59 +0300

    Автомат по продаже жевачек-шариков

--- _10.State/Program.cs
+++ _10.State/Program.cs
@@ -6,7 +6,43 @@ namespace _10.State
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TryGumballMachine();
+        }
+
+        static void TryGumballMachine()
+        {
+            var gumbalMachine = new GumballMachine(5);
+
+            Console.WriteLine(gumbalMachine);
+
+            gumbalMachine.InsertQuarter();
+            gumbalMachine.TurnCrank();
+
+            Console.WriteLine(gumbalMachine);
+
+            gumbalMachine.InsertQuarter();
+            gumbalMachine.EjectQuarter();
+            gumbalMachine.TurnCrank();
+
+            Console.WriteLine(gumbalMachine);
+
+            gumbalMachine.InsertQuarter();
+            gumbalMachine.TurnCrank();
+            gumbalMachine.InsertQuarter();
+            gumbalMachine.TurnCrank();
+            gumbalMachine.EjectQuarter();
+
+            Console.WriteLine(gumbalMachine);
+
+            gumbalMachine.InsertQuarter();
+            gumbalMachine.InsertQuarter();
+            gumbalMachine.TurnCrank();
+            gumbalMachine.InsertQuarter();
+            gumbalMachine.TurnCrank();
+            gumbalMachine.InsertQuarter();
+            gumbalMachine.TurnCrank();
+
+            Console.WriteLine(gumbalMachine);
         }
     }
 }
